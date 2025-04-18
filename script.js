e1008131e7cbde4e55960b401f71d2623a403fd0
Dale-jpg <echols.dale@gmail.com>
2025-04-18 16:08:32 -0400

    Created a for loop to create a 16x16 grid
    the loop avoided manually entering it all in
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   index.html
    	modified:   script.js
    	modified:   style.css

---
+++ script.js
@@ -0,0 +1,16 @@
+// Javascript File
+const container = document.querySelector('#container');
+
+for (i=0;i < 256; i++) {
+    const div = document.createElement('div');
+    div.className = "grid";
+
+    container.appendChild(div);
+
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = "blue";
+    
+    })
+}
+
+
