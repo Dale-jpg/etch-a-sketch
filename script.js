38deed9a3b607d46cde524ba90415f3c1b4c299b
Dale-jpg <echols.dale@gmail.com>
2025-04-18 16:34:09 -0400

    Created function that will be called on when button is pushed.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   script.js
    	modified:   style.css

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
 // Javascript File
 const container = document.querySelector('#container');
+const button = document.querySelector("button");
+let sizeInput;
+let size;
+let maxNumber = 100;
+let grid;
 
-for (i=0;i < 256; i++) {
-    const div = document.createElement('div');
-    div.className = "grid";
+// for (i=0;i < 256; i++) {
+//     const div = document.createElement('div');
+//     div.className = "grid";
 
-    container.appendChild(div);
+//     container.appendChild(div);
 
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = "blue";
-    })
+//     div.addEventListener('mouseover', () => {
+//         div.style.backgroundColor = "blue";
+//     })
+// }
+
+
+
+
+function resizeGrid () {
+    while (true) {
+    sizeInput = prompt("Please enter the number of squares per side");
+
+    if (sizeInput === null) {
+        alert("Prompt cancelled by user");
+        break;
+    }
+
+    size = Number(sizeInput);
+
+    if (!isNaN(size) && size <= maxNumber) {
+        grid = size * size;
+        for (i=0;i < grid; i++) {
+            const div = document.createElement('div');
+            div.className = "grid";
+        
+            container.appendChild(div);
+        
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = "blue";
+            })
+        }
+        break;
+    } else {
+        alert(`Invalid input. Please enter a number less than or equal to ${maxNumber}.`);
+    }
+    }
+    
 }
-
 
